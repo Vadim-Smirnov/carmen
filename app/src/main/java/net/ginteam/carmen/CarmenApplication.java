ffc6f22ac51c6315941ae013936c8302bd65cde3
vadik <fearlstaruy@mail.ru>
2016-12-22 17:56:28 +0200

    added navigation menu

--- app/src/main/java/net/ginteam/carmen/CarmenApplication.java
+++ app/src/main/java/net/ginteam/carmen/CarmenApplication.java
@@ -2,9 +2,21 @@ package net.ginteam.carmen;
 
 import android.app.Application;
 
+import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
+
 /**
  * Created by Eugene on 12/21/16.
  */
 
 public class CarmenApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
+                .setDefaultFontPath(getString(R.string.open_sans_light_font))
+                .setFontAttrId(R.attr.fontPath)
+                .build());
+    }
 }
---
+++ app/src/main/java/net/ginteam/carmen/view/activity/MainActivity.java
@@ -0,0 +1,24 @@
+package net.ginteam.carmen.view.activity;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import net.ginteam.carmen.R;
+
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
+    }
+
+}
