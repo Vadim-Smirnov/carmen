6a452c975024ea6fe51653853efcf2c547981924
Eugene <shcherbinock95@gmail.com>
2016-12-25 19:36:09 +0200

    separate resources for categories

--- app/src/main/java/net/ginteam/carmen/view/activity/MainActivity.java
+++ app/src/main/java/net/ginteam/carmen/view/activity/MainActivity.java
@@ -25,7 +25,6 @@ public class MainActivity extends AppCompatActivity implements CategoryListFragm
     private DrawerLayout mDrawerLayout;
     private NavigationView mNavigationView;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
