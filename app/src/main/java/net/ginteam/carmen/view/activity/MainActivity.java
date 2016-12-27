ff944e36e85579118287ab891ae307136c2464ef
Eugene <shcherbinock95@gmail.com>
2016-12-27 19:37:53 +0200

    clean main activity

--- app/src/main/java/net/ginteam/carmen/view/activity/MainActivity.java
+++ app/src/main/java/net/ginteam/carmen/view/activity/MainActivity.java
@@ -1,117 +1,31 @@
 package net.ginteam.carmen.view.activity;
 
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.design.internal.NavigationMenuView;
-import android.support.design.widget.NavigationView;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.MenuItem;
 import android.widget.Toast;
 
 import net.ginteam.carmen.R;
-import net.ginteam.carmen.contract.MainContract;
 import net.ginteam.carmen.model.category.CategoryModel;
-import net.ginteam.carmen.presenter.MainPresenter;
-import net.ginteam.carmen.view.custom.ToolbarDrawerToggle;
+import net.ginteam.carmen.model.city.CityModel;
 import net.ginteam.carmen.view.fragment.category.CategoryListFragment;
+import net.ginteam.carmen.view.fragment.city.CityListFragment;
 
-import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
-
-public class MainActivity extends AppCompatActivity implements MainContract.View, CategoryListFragment.OnCategorySelectedListener, NavigationView.OnNavigationItemSelectedListener {
-
-    private MainContract.Presenter mPresenter;
-
-    private Toolbar mToolbar;
-    private DrawerLayout mDrawerLayout;
-    private NavigationView mNavigationView;
+public class MainActivity extends NavigationViewActivity implements CategoryListFragment.OnCategorySelectedListener,
+        CityListFragment.OnCitySelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        initializeToolbar();
-        initializeNavigationView();
-
-        mPresenter = new MainPresenter(this);
-        mPresenter.getLastLocation();
+        super.setContentView(R.layout.activity_main);
     }
 
     @Override
-    public void onBackPressed() {
-        if (mDrawerLayout.isDrawerOpen(GravityCompat.START)) {
-            mDrawerLayout.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
-    }
-
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        return true;
-    }
-
-    @Override
-    public void onCategorySelect(CategoryModel category) {
+    public void onCategorySelected(CategoryModel category) {
         Toast.makeText(this, category.getName(), Toast.LENGTH_SHORT).show();
     }
 
     @Override
-    protected void attachBaseContext(Context newBase) {
-        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
+    public void onCitySelected(CityModel city) {
+        Toast.makeText(this, city.getName(), Toast.LENGTH_SHORT).show();
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        mPresenter.onActivityResult(requestCode, resultCode, data);
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        mPresenter.onRequestPermissionsResult(requestCode,permissions,grantResults);
-    }
-
-    @Override
-    public AppCompatActivity getActivity() {
-        return this;
-    }
-
-    @Override
-    public Context getContext() {
-        return this;
-    }
-
-    private void initializeToolbar() {
-        mToolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(mToolbar);
-    }
-
-    private void initializeNavigationView() {
-        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        mNavigationView = (NavigationView) findViewById(R.id.navigation_view);
-
-        ActionBarDrawerToggle toggle = new ToolbarDrawerToggle(this, mDrawerLayout, mToolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        toggle.syncState();
-
-        mDrawerLayout.addDrawerListener(toggle);
-        mNavigationView.setNavigationItemSelectedListener(this);
-        disableNavigationViewScrollbars(mNavigationView);
-    }
-
-    private void disableNavigationViewScrollbars(NavigationView navigationView) {
-        if (navigationView != null) {
-            NavigationMenuView navigationMenuView = (NavigationMenuView) navigationView.getChildAt(0);
-            if (navigationMenuView != null) {
-                navigationMenuView.setVerticalScrollBarEnabled(false);
-            }
-        }
-    }
 }
