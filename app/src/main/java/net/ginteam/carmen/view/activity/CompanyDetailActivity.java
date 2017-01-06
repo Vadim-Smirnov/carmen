2a4ce8a6e7066302264466dc4366600092e98c76
Eugene <shcherbinock95@gmail.com>
2017-01-06 15:07:55 +0200

    update detail activity

--- app/src/main/java/net/ginteam/carmen/view/activity/CompanyDetailActivity.java
+++ app/src/main/java/net/ginteam/carmen/view/activity/CompanyDetailActivity.java
@@ -1,17 +1,24 @@
 package net.ginteam.carmen.view.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.widget.Toast;
 
 import net.ginteam.carmen.R;
 
 public class CompanyDetailActivity extends ToolbarActivity {
 
+    public static final String ARGUMENTS_EXTRA = "company_information";
+    public static final String COMPANY_ARGUMENT = "company";
+
+    private int mCompanyId;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_company_detail);
+
+        receiveArguments();
     }
 
     @Override
@@ -19,4 +26,13 @@ public class CompanyDetailActivity extends ToolbarActivity {
         getMenuInflater().inflate(R.menu.company_list_menu, menu);
         return true;
     }
+
+    private void receiveArguments() {
+        Bundle arguments = getIntent().getBundleExtra(ARGUMENTS_EXTRA);
+        if (arguments != null) {
+            mCompanyId = arguments.getInt(COMPANY_ARGUMENT, 0);
+            Toast.makeText(this, "Receive ID: " + mCompanyId, Toast.LENGTH_LONG).show();
+        }
+    }
+
 }
